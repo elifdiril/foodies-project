adf3da357cc59fd85f3fd972849fe66d4997dc56
Elif Gedik <elifdiril6@gmail.com>
2024-01-24 21:31:55 +0300

    style changes

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Share = () => {
   return (
-    <div className="flex flex-col justify-between bg-[#ffdec5]">
+    <div className="flex flex-col justify-between bg-[#fff]">
       <Header />
-      <div className="flex items-center justify-center p-10 min-h-[calc(100vh-80px)] font-bold font-mono text-[#A73121]">
+      <div className="flex items-center justify-center p-10 min-h-[calc(100vh-80px)] font-bold font-mono text-[#402714]">
         Coming Soon
       </div>
       <Footer />
