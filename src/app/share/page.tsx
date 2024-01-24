ae923ae7128ab3757dc97381375f76b3735e8dc4
Elif Gedik <elifdiril6@gmail.com>
2024-01-24 20:50:20 +0300

    share page added

---
+++ src/app/share/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/header/Footer";
+import Header from "@/components/header/Header";
+import React from "react";
+
+const Share = () => {
+  return (
+    <div className="flex flex-col justify-between bg-[#FF9B50]">
+      <Header />
+      <div className="flex items-center justify-center p-10 min-h-[calc(100vh-80px)] font-bold font-mono text-[#A73121]">
+        Coming Soon
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Share;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,27 @@
-import Link from 'next/link';
-import React from 'react';
+import { importImages } from "@/utils/importImages";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className='flex items-center gap-4 text-lg text-[#FFBB5C] justify-end bg-[#C63D2F] px-10 h-10'>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center text-lg text-[#FFBB5C] bg-[#C63D2F] px-10 h-10">
+      <Link href="/">
+        <Image
+          src={importImages("healthy-food.jpg")}
+          alt="logo"
+          width={60}
+          className="rounded-md"
+        />
+      </Link>
+      <Link href="/share" className="hover:opacity-65">Share a Food!</Link>
+      <div className="flex items-center gap-4 text-lg justify-end">
+        <Link className="hover:opacity-65" href="/">Home</Link>
+        <Link className="hover:opacity-65" href="/about">About</Link>
+        <Link className="hover:opacity-65" href="/contact">Contact</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
